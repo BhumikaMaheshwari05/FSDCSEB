0b88a59e972ef1627ca172ca2997df633d1b848f
BhumikaMaheshwari05 <maheshwaribhumika82@gmail.com>
2024-10-17 14:13:33 +0530

    About DOM

--- JSWork/index.js
+++ JSWork/index.js
@@ -92,12 +92,80 @@
 // let data=selectLanguage("c");
 // console.log(data);
 
-// console.log("hello")
+//  console.log("hello")
 // let parent=document.getElementsByClassName("parent");
 // console.log(parent);
 // parent[0].innerText="Data has changed";
 
- console.log("hello")
- let parent=document.getElementsByClassName("parent");
-  console.log(parent);
-  parent[0].innerHTML="<h2>Data has changed</h2>";
+//  console.log("hello")
+//  let parent=document.getElementsByClassName("parent");
+//  console.log(parent);
+// //  parent[0].innerHTML="<h2 style=color:Red>Data has changed</h2>";
+// const h1=document.createElement("h1");
+// console.log(h1);
+// h1.innerText="ABES Engineering College";
+// console.log(h1);
+// h1.style.backgroundColor="cyan"
+// h1.style.color="red";
+// h1.style.padding="20px";
+// h1.style.margin="10px"
+// parent[0].appendChild(h1);
+
+// const img=document.createElement("img");
+// img.src="./flower.webp";
+// console.log(img);
+// parent[0].appendChild(img);
+// img.setAttribute("height","300");
+// img.setAttribute("width","300")
+// parent[0].appendChild(img);
+// parent[0].removeChild(img)
+
+// function getData(){
+//     console.log("Hi,inside getdata function");
+//     parent[0].innerHTML="<h2 style=color:red>Hello,EventHandled</h2>";
+// }
+
+// const button=document.getElementsByClassName("btn");
+// console.log(button);
+// button[0].addEventListener("click",getData);
+
+// const promise=new Promise((resolve,reject)=>
+// {
+//           let a=12;
+//           if(a>10){
+//             resolve("Accepted");
+//           }
+//           else{
+//             reject("Not Accepted");
+//           }
+// })
+
+// promise.then((msg)=>
+// {
+//     console.log(msg);
+// })
+// .catch((error)=>
+//     {console.log(error);
+
+//  })
+// .finally(()=>console.log("ALL resourses have closed"));
+
+// const promise2= new Promise((resolve,reject)=>
+// {
+//     resolve({name:"Bhumika",Stack:"Mern Stack"});
+// })
+// promise2.then((data)=>{
+//     console.log("Hi,"+data.name);
+// })
+// .catch(x=>{
+//     console.log(x);
+// })
+
+const response=fetch("https://dummyjson.com/products");
+response.then((data)=>{
+    console.log(data);
+    data.json().then((res)=>
+    {
+        console.log(res);
+    })
+})
