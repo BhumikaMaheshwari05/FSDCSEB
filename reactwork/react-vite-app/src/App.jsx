ae0df394aa4a2657349e491e540d277f2a456148
BhumikaMaheshwari05 <maheshwaribhumika82@gmail.com>
2024-11-14 13:08:15 +0530

    Introduction to Hooks

--- reactwork/react-vite-app/src/App.jsx
+++ reactwork/react-vite-app/src/App.jsx
@@ -1,40 +1,79 @@
 import React from 'react'
 import Student from './Student';
 import logo from "./assets/img.png"
+import UseStudentState from './UseStudentState';
 function App() {
   const h1=<h1>Hello World</h1>;
   const mystyle={
     color:'red',
     backgroundColor:'yellow'
   }
-  return (
-    <div style={{backgroundColor:'cyan'}}>
-      {h1}
-      <div style={mystyle}>
-      ABES Engineering College
-      </div>
+  const Studentdata=[{
+    college:"ABES Engineering College",
+    name:"Rahul",
+    branch:"CSE",
+    sec:"A",
+    roll:24650,
+    
+},
+{
+  college:"ABES Engineering College",
+  name:"Riya",
+  branch:"CSE",
+  sec:"A",
+  roll:24622,
+  
+},
+{
+  college:"ABES Engineering College",
+  name:"Simran",
+  branch:"CSE",
+  sec:"A",
+  roll:24634,
+  
+}
+
+]
+    // <div style={{backgroundColor:'cyan'}}>
+    //   {h1}
+    //   <div style={mystyle}>
+    //   <center>ABES Engineering College</center>
+    //   </div>
+    //   <div style={{display:'flex',justifyContent:'center'}}>
+    //   {Studentdata.map((ele)=>{
+    //     return <Student data={ele}/>
+
+    //   })
+    //       }
+    //     {/* <Student data={Studentdata[0]}/> */}
+    //     {/* <center><Student college="ABES Engineering College"
+    //     name="Bhumika"
+    //     pic={<img src={logo} height={100} width={100} style={{marginRight:'20px'}}/>}
+    //     branch="CSE"
+    //     roll="50"
+    //     sec="B"
+    //     /></center>
+    //     <center><Student college="ABES Engineering College"
+    //     name="Riya"
+    //     branch="CS"
+    //     roll="33"
+    //     sec="A"
+    //     /></center>
+    //     <center><Student college="ABES Engineering College"
+    //     name="Rahul"
+    //     branch="CSE-AIML"
+    //     roll="47"
+    //     sec="C"
+    //     /></center> */}
+    //   </div>
+    // </div>
+    return (
       <div>
-        <Student college="ABES Engineering College"
-        name="Bhumika"
-        pic={<img src={logo} height={100} width={100}/>}
-        branch="CSE"
-        roll="50"
-        sec="B"
-        />
-           <Student college="ABES Engineering College"
-        name="Riya"
-        branch="CS"
-        roll="33"
-        sec="A"
-        />
-           <Student college="ABES Engineering College"
-        name="Rahul"
-        branch="CSE-AIML"
-        roll="47"
-        sec="C"
-        />
+        <h2>Hello Using useState Hook</h2>
+        <div>
+          <UseStudentState/>
+        </div>
       </div>
-    </div>
     
   )
 }
--- reactwork/react-vite-app/src/Student.jsx
+++ reactwork/react-vite-app/src/Student.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import './student.css'
+import logo from "./assets/img.png"
 
-function Student(props) {
+function Student({data}) {
   return (
     <div className='icard'>
+      {/* {
+        JSON.stringify(data)
+      } */}
       <table>
        <tbody>
         <tr>
-          <td colSpan={2}>{props.college}
+          <td colSpan={2}>{data.college}
            
           </td></tr>
-          <tr><td>{props.pic}</td></tr>
-          <tr><td>Name</td><td>{props.name}</td></tr>
-          <tr><td>Branch</td><td>{props.branch}</td></tr>
-          <tr><td>Roll_Number</td><td>{props.roll}</td></tr>
-          <tr><td>Section</td><td>{props.sec}</td></tr>
+          <tr><td>Pic</td><td>{data.pic}</td></tr>
+          <tr><td>Name</td><td>{data.name}</td></tr>
+          <tr><td>Branch</td><td>{data.branch}</td></tr>
+          <tr><td>Roll_Number</td><td>{data.roll}</td></tr>
+          <tr><td>Section</td><td>{data.sec}</td></tr>
          
        </tbody>
       </table>
       </div>
   )
 }
+// Student.defaultProps={
+//   college:"AKG Engineering College"
+// }
 export default Student
